27ba5a0d66b16d0a88318abbcc70dd812f2e6417
Andres Alvarez <andrew.cei@comunidad.unam.mx>
2024-07-01 20:19:10 -0600

    Primer router completo y funcional

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
 require '../helpers.php';
+require basePath('Router.php');
 
-require basePath('views/home.view.php');
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
